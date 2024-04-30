f65364d3dc33e9abd5378386b7ab6345ec340a8a
Sam Sneed <163201376+sam-sneed@users.noreply.github.com>
2024-04-30 20:30:47 +0000

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,1 +1,3 @@
+sudo apt install flatpak gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install librewolf shotcut tutanota geforce krita libreoffice freetube spotify vscodium vinegar com.obsproject.Studio bottles
